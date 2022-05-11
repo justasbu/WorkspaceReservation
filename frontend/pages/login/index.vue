94b338fa98df47b42285cc8784011c42ef095760
UC-0583\UC-0583 <justas.bucionis@gmail.com>
2022-05-11 11:32:19 +0300

    fixed warnings

--- frontend/pages/login/index.vue
+++ frontend/pages/login/index.vue
@@ -4,7 +4,7 @@
       <div>
         <v-tabs v-model="tab" show-arrows background-color="deep-purple accent-4" icons-and-text dark grow>
           <v-tabs-slider color="purple darken-4"></v-tabs-slider>
-          <v-tab v-for="i in tabs">
+          <v-tab v-for="i in tabs" v-bind:key="i.name">
             <v-icon large>{{ i.icon }}</v-icon>
             <div class="caption py-1">{{ i.name }}</div>
           </v-tab>
